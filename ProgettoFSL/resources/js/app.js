e164c7e42ad9a7931f0f7ce6e6307828c4285ca9
BulaCibula03 <bulacibula03@proton.me>
2026-01-27 11:37:19 +0100

    Project fix

--- ProgettoFSL/resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './components/App.vue';
-import router from './router';
-import './bootstrap';
-
-
-createApp(App).use(router).mount('#app');
--- README.md
+++
@@ -1,2 +0,0 @@
-# ProgettoFSL
-Webapp di gestione delle attività di FSL (Formazione Scuola Lavoro) e dei corsi di sicurezza
