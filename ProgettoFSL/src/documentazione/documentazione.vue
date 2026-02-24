2aa0859164bd5010f9f48df4d892dde2209f135a
-g <-g>
2026-02-24 12:05:09 +0100

    documentazione

---
+++ ProgettoFSL/src/documentazione/documentazione.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="documentazione">
+        <h1>Documentazione</h1> <!--Funzionalità, introduzione-->
+        <p>
+            Benvenuto nella documentazione del progetto FSL Galileo.</br>
+            In questa pagina troverai tutte le informazioni di funzionamento del progetto.</br> 
+        </p>
+        <h2>Funzionalità</h2>
+        <p>
+            Il progetto FSL Galileo è un programma per la gestione degli dati anagrafici della scuola e dei documenti FSL oltre anche quelli dei corsi scolastici.</br>
+            Le principali funzionalità includono:</br>
+            - Gestione degli studenti: inserimento, modifica, eliminazione e visualizzazione dei dati anagrafici degli studenti.</br>
+            - Gestione degli docenti: inserimento, modifica, eliminazione e visualizzazione dei dati anagrafici degli docenti.</br>
+            - Gestione dei dati per documenti FSL: inserimento, modifica, eliminazione e visualizzazione dei documenti FSL associati agli studenti.</br>
+            - Gestione dei dati per corsi: inserimento, modifica, eliminazione e visualizzazione dei corsi scolastici frequentati dagli studenti.</br>
+        </p>
+        <h2>Interfaccia utente</h2>
+        <img src="framework.jpeg" alt="Framework utilizzato" class="img-documentazione"><br>
+        <p>
+            Questa è la schermata delle tabelle caricate dal database su cui in base al livello di accesso è possibile effettuare operazioni specifiche.<br>
+        </p>
+        <img src="tabelle.jpeg" alt="Tabelle del database" class="img-documentazione"><br>
+        <p>
+            Questa è un menu a cascata che permette di visualizzare le tabelle dei dati.<br>
+        </p>
+        <img src="cerca.jpeg" alt="Barra di ricerca" class="img-documentazione"><br>
+        <p>
+            Questa è la barra di ricerca che permette di cercare in base a un criterio nella tabella selezionata.<br>
+        </p>
+        <img src="apertura.jpeg" alt="Apertura del database" class="img-documentazione"><br>
+        <p>
+            Questa è la sidebar che apre il menu a cascata per selezionare una tabella.<br>
+        </p>
+    </div>
+</template>
